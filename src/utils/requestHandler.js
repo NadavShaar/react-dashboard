c2b356d0cd27179c1de7352c40495def4bfdf189
Nadav Shaar <nadavshaar@gmail.com>
2020-09-05 17:17:13 +0300

    heroku update.

--- src/utils/requestHandler.js
+++ src/utils/requestHandler.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 export default function requestHandler({requestType, action, data}) {
 
-    return axios[requestType || 'post'](`http://localhost:3000/${action}`, data)
+    return axios[requestType || 'post'](`https://shielded-wildwood-86962.herokuapp.com/${action}`, data)
         .catch(err => {
             console.error(err);
             throw err;
