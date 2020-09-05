347cdcc15fcf91f0bf18c8833e079b6083e242fb
Nadav Shaar <nadavshaar@gmail.com>
2020-09-05 17:27:28 +0300

    update.

--- src/utils/requestHandler.js
+++ src/utils/requestHandler.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 export default function requestHandler({requestType, action, data}) {
 
-    return axios[requestType || 'post'](`https://shielded-wildwood-86962.herokuapp.com/${action}`, data)
+    return axios[requestType || 'post'](`https://react-dashboard-server.herokuapp.com/${action}`, data)
         .catch(err => {
             console.error(err);
             throw err;
